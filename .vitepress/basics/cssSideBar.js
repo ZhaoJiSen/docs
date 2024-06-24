3213c91ff0ad3ca1f8b946b8cdb63f7e752b18b9
zhaojisen <1301338853@qq.com>
2024-06-24 14:07:50 +0800

    tailwind 笔记

--- .vitepress/basics/cssSideBar.js
+++ .vitepress/basics/cssSideBar.js
@@ -3,6 +3,8 @@ import { walk } from '../scripts/util';
 const baseDir = './basics/CSS/';
 
 export const basicsCSSSideBar = [
+  walk(baseDir, 'Flex 布局', false, false),
+  walk(baseDir, 'Grid 布局', false, false),
   walk(baseDir, 'CSS预处理器', false, false),
   walk(baseDir, 'CSS后处理器', false, false),
   walk(baseDir, 'TailwindCSS', false, false),
